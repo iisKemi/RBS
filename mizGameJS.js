7c5f873ee452b54b3541720276802810952ae59c
iiskemi <snguyen4@worcester.edu>
2022-07-28 17:16:35 -0400

    V2.0

--- mizGameJS.js
+++ mizGameJS.js
@@ -1,7 +1,17 @@
-let p1Pick, whoKilled,p2Pick;
-let p1ammo = 0;
-let p2ammo = 0;
+let p1Pick, whoKilled,p2Pick,cpuPick;
+let p1Ammo = 0;
+let p2Ammo = 0;
 let turn = 0;
+
+let p1Log = document.getElementById("p1Log");
+
+let p1ScorePrint = document.getElementById("score1");
+let p2ScorePrint = document.getElementById("score2");
+
+const shoot = 0;
+const block = 1;
+const reload = 2;
+const noAmmo = 3;
 
 
 let p1Score = 0;
@@ -10,82 +20,55 @@ let p2Score = 0;
 
 
 function selectShoot() {
-	
+	p1Pick = shoot;
 	cpuPick = cpu();
-	if(p1ammo == 0) {
-		console.log("No ammo!")
-		if (cpuPick == 0) {
-			console.log("P2 shoots you!");
-			document.getElementById("logs").innerHTML = "No ammo! : P2 shoots you!";
-			death(2);
-		}
-		else if (cpuPick == 1) {
-			console.log("P2 Blocked!")
-			document.getElementById("logs").innerHTML = "No ammo! : P2 Blocked!";
-			}
-		else if (cpuPick == 2) {
-			console.log("P2 Reloaded!");
-			document.getElementById("logs").innerHTML = "No ammo! : P2 Reloaded!";
-			
-			}
-	}
-		else {
-			p1ammo -= 1;
-			if (cpuPick == 0) {
-				console.log("You shot each other!");
-				document.getElementById("logs").innerHTML = "You shot each other!";
-				death(0);
-			}
-			else if (cpuPick == 1) {
-				console.log("P2 Blocked!")
-				document.getElementById("logs").innerHTML = "P2 Blocked!";
-			}
-			else if (cpuPick == 2) {
-				console.log("P2 Killed while reloading!");
-				document.getElementById("logs").innerHTML = "P2 Killed while reloading!";
-				death(1);
-			}
-		}
-	}
+	p1Shoot(p1Pick, cpuPick);
+}
 	
 
 function selectBlock() {	
+	p1Pick = block;
 	cpuPick = cpu();
-	console.log(cpuPick + " Block");
-	if (cpuPick == 0) {
-		console.log("Blocked p2 shot!");
-		document.getElementById("logs").innerHTML = "Blocked p2 shot!";
-	}
-	else if (cpuPick == 1) {
-		console.log("Both Blocked!")
-		document.getElementById("logs").innerHTML = "Both Blocked!";
-	}
-	else if (cpuPick == 2) {
-		console.log("P2 Reloaded!");
-		document.getElementById("logs").innerHTML = "P2 Reloaded!";
-	}
-	
+	p1Log.innerHTML = "Player 1 blocked!";
 }
 
 function selectReload() {
+	p1Pick = reload;
 	cpuPick = cpu();
-	p1ammo += 1;
-	if (cpuPick == 0) {
-		console.log("P2 shoots you!");
-		document.getElementById("logs").innerHTML = "P2 shoots you!";
-		death(2);
-	}
-	else if (cpuPick == 1) {
-		console.log("P2 blocked!")
-		document.getElementById("logs").innerHTML = "P2 blocked!";
-	}
-	else if (cpuPick == 2) {
-		console.log("P2 Reloaded!");
-		document.getElementById("logs").innerHTML = "P2 Reloaded!";
-	}
-	
+	p1Reload(p1Pick, cpuPick);
 }
 
+function p1Shoot(p1Pick,cpuPick) {
+	if(p1Ammo >= 1) {
+		p1Log.innerHTML = "Player 1 shoots!";
+		if(p1Pick == shoot && cpuPick == shoot) {
+			death(0);
+		}
+		else if(p1Pick == shoot && cpuPick == block) {
+			p1Ammo -= 1;
+		}
+		else if(p1Pick == shoot && (cpuPick == reload || cpuPick == noAmmo)) {
+			death(1);
+		}
+	}
+	else if(p1Ammo == 0) {
+		p1Log.innerHTML = "Player 1 tried to shoot without ammo!";
+		if(p1Pick == shoot && cpuPick == shoot) {
+			death(2);
+		}
+	}
+}
+
+function p1Reload(p1Pick, cpuPick) {
+	if(p1Pick == reload && cpuPick == shoot) {
+		death(2);
+	}
+	else if(p1Pick == reload && (cpuPick == block || cpuPick == reload)) {
+		p1Log.innerHTML = "Player 1 reloaded!";
+		p1Ammo += 1;
+		console.log(p1Ammo);
+	}
+}
 
 function death(whoKilled) {
 	if (whoKilled == 1) {
@@ -99,31 +82,40 @@ function death(whoKilled) {
 		p2Score += 1;
 	}
 	
-	document.getElementById("score1").innerHTML = "Player 1 Score : " + p1Score;
-	document.getElementById("score2").innerHTML = "Player 2 Score : " + p2Score;
+	p1ScorePrint.innerHTML = "Player 1 Score : " + p1Score;
+	p2ScorePrint.innerHTML = "Player 2 Score : " + p2Score;
 	
-	console.log("Player 1 Score : " + p1Score);
-	console.log("Player 2 Score : " + p2Score);
-	p1ammo = 0;
-	p2ammo = 0;
+	p1Ammo = 0;
+	p2Ammo = 0;
 }
 
 function cpu() {
-	if (p2ammo > 0) {
-		p2Pick = Math.floor(Math.random() * 3);
-		if(p2Pick == 0) {
-			p2ammo -= 1;
+	const p2Log = document.getElementById("p2Log");
+	
+	if (p2Ammo == 0) {
+		p2Pick = Math.floor(Math.random() * 2 + 1);
+		if(p2Pick == reload) {
+			p2Ammo += 1;
+			p2Log.innerHTML = "Player 2 reloaded!";
+			return p2Pick;
 		}
-		else if(p2Pick == 2) {
-			p2ammo += 1;
-		}
+		p2Log.innerHTML = "Player 2 blocked!";
 		return p2Pick;
 	}
-	else if (p2ammo == 0) {
-		p2Pick = Math.floor(Math.random() * 2 + 1);
-		if(p2Pick == 2) {
-			p2ammo += 1;
+	else if (p2Ammo > 0) {
+		p2Pick = Math.floor(Math.random() * 3);
+		if(p2Pick == shoot) {
+			p2Ammo -= 1;
+			p2Log.innerHTML = "Player 2 shoots!";
+			return p2Pick;
 		}
+		else if(p2Pick == reload) {
+			p2Ammo += 1;
+			p2Log.innerHTML = "Player 2 reloaded!";
+			return p2Pick;
+		}
+		p2Log.innerHTML = "Player 2 blocked!";
 		return p2Pick;
-		}
+	}
 }
+
